932cebf6659aa5969f591405233b96f8988b62d8
ZhenjaMax <54914800+ZhenjaMax@users.noreply.github.com>
2019-10-01 02:54:58 +0300

    prog python laba

---
+++ laba.py
@@ -0,0 +1,38 @@
+import wikipedia
+
+pages_list = str(input())
+pages_list = pages_list.split(', ')
+
+if pages_list[-1] in wikipedia.languages().keys:
+    wikipedia.set_lang(pages_list[-1])
+    del pages_list[-1]
+else:
+    print('no results')
+    exit
+
+max_words, page_number_max = 0, 0
+
+for i in range(len(pages_list)):
+    text = wikipedia.summary(pages_list[i])
+    words = text.count(' ') + 1
+    if words >= max_words:
+        max_words = words
+        page_number_max = i
+print(wikipedia.page(pages_list[page_number_max]).title, max_words)
+
+chain = []
+
+for i in pages_list:
+    links = wikipedia.page(i).links
+    pages_list.remove(i)
+    for j in links:
+        if j in pages_list:
+            chain_add = [i, j]
+            chain.append(chain_add)
+        else:
+            sublinks = wikipedia.page(j).links
+            for k in sublinks:
+                if k in pages_list:
+                    chaid_add = [i, j, k]
+                    chain.append(chain_add)
+print(chain)
