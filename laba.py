3523df8a7a15dae38daa2cfa5fe003d6361c67c7
ZhenjaMax <54914800+ZhenjaMax@users.noreply.github.com>
2019-10-01 14:48:40 +0300

    Update laba.py

--- laba.py
+++ laba.py
@@ -1,38 +1,38 @@
-import wikipedia
-
-pages_list = str(input())
-pages_list = pages_list.split(', ')
-
-if pages_list[-1] in wikipedia.languages().keys:
-    wikipedia.set_lang(pages_list[-1])
-    del pages_list[-1]
-else:
-    print('no results')
-    exit
-
-from help_wiki_function import is_page_valid
-for i in pages_list:
-    if not age_valid(i):
-        pages_list.remove(i)
-
-max_words, page_number_max = 0, 0
-for i in range(len(pages_list)):
-    text = wikipedia.summary(pages_list[i])
-    words = text.count(' ') + 1
-    if words >= max_words:
-        max_words = words
-        page_number_max = i
-print(wikipedia.page(pages_list[page_number_max]).title, max_words)
-
-chain=[]
-chain.append(pages_list[0])
-for i in range(len(pages_list)-1):
-    if pages_list[i+1] in wikipedia.page(pages_list[i]).links:
-        chain.append(pages_list[i+1])
-    else:
-        sublinks=wikipedia.page(pages_list[i]).links
-        for j in sublinks:
-            if pages_list[i+1] in wikipedia.page(j).links:
-                chain.append(j)
-                chain.append(pages_list[i+1])
+import wikipedia                                                                
+                                                                                
+pages_list = str(input())                                                       
+pages_list = pages_list.split(', ')                                             
+                                                                                
+if pages_list[-1] in wikipedia.languages():                                     
+    wikipedia.set_lang(pages_list[-1])                                          
+    del pages_list[-1]                                                          
+else:                                                                           
+    print('no results')                                                         
+    exit()                                                                      
+                                                                                
+from help_wiki_function import is_page_valid                                    
+for i in pages_list:                                                            
+    if not is_page_valid(i):                                                    
+        pages_list.remove(i)                                                    
+                                                                                
+max_words, page_number_max = 0, 0                                               
+for i in range(len(pages_list)):                                                
+    text = wikipedia.summary(pages_list[i])                                     
+    words = text.count(' ') + 1                                                 
+    if words >= max_words:                                                      
+        max_words = words                                                       
+        page_number_max = i                                                     
+print(wikipedia.page(pages_list[page_number_max]).title, max_words)             
+                                                                                
+chain=[]                                                                        
+chain.append(pages_list[0])                                                     
+for i in range(len(pages_list)-1):                                              
+    if pages_list[i+1] in wikipedia.page(pages_list[i]).links:                  
+        chain.append(pages_list[i+1])                                           
+    else:                                                                       
+        sublinks=wikipedia.page(pages_list[i]).links                            
+        for j in sublinks:                                                      
+            if pages_list[i+1] in wikipedia.page(j).links:                      
+                chain.append(j)                                                 
+                chain.append(pages_list[i+1])                                   
 print(chain)
