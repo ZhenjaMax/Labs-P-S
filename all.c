1ae71ec556c89e2f0142768bfa205c9aa3e50be0
ZhenjaMax <54914800+ZhenjaMax@users.noreply.github.com>
2019-12-03 01:18:48 +0300

    Update all.c

--- all.c
+++ all.c
@@ -5,40 +5,43 @@
 
 char** readText(int *sentence_count){
 	char **text = (char**)calloc(1, sizeof(char*));
-	if (text == NULL)
-		return NULL;
-	int i=0;
+    if (text == NULL)
+        return NULL;
+    int i=0;
+    char **buffer_text;
+	char *buffer_sentence;
 	int sentence_max=0;
-	char c = getchar();
-	do{
-		i=0;
-		if((text=(char**)realloc(text, (*sentence_count+1)*sizeof(char**)))==NULL)
-			return NULL;
-		do{
-			if((text[i]=(char*)realloc(text[*sentence_count], sizeof(char*)*(i+3)))==NULL)
-				return NULL;
-			text[*sentence_count][i]=c;
-			i++;
-		}while((c=getchar())!='.');
-		text[*sentence_count][i]='.';
-		text[*sentence_count][i+1]='\0';
-		(*sentence_count)++;
-		if(i+2>sentence_max)
+    char c = getchar();
+    do{
+        i=0;
+        if((buffer_text=(char**)realloc(text, ((*sentence_count)+1)*sizeof(char**)))==NULL)
+            return NULL;
+        text=buffer_text;
+        do{
+			if ((buffer_sentence=(char*)realloc(text[*sentence_count], sizeof(char*)*(i+3)))==NULL)
+    			return NULL;
+    		text[*sentence_count]=buffer_sentence;
+            text[*sentence_count][i]=c;
+            i++;
+        }while((c=getchar())!='.');
+        text[*sentence_count][i]='.';
+        text[*sentence_count][i+1]='\0';
+        (*sentence_count)++;
+		if (i+2>sentence_max)
 			sentence_max=i+2;
-	}while((c = getchar())!='\n');
-	
-	char checker[*sentence_count][sentence_max];
-	int j=0;
-	
+    }while((c = getchar())!='\n');
+    
+    char checker[*sentence_count][sentence_max];
+    int j;
+    
 	for(i=0; i<*sentence_count; i++){
 		strcpy(checker[i], text[i]);
 		for(j=0; checker[i][j]; j++)
-			checker[i][j]=tolower(checker[i][j]);
+		    checker[i][j]=tolower(checker[i][j]);
 	}
-	for(i=0;i<*sentence_count;i++)
+    for(i=0;i<*sentence_count;i++)
 		for(j=i+1; j<*sentence_count; j++)
 			if(!strcmp(checker[i], checker[j])){
-				free(text[j]); 	/////////////////
 				(*sentence_count)--;
 				for(int k=j; k<*sentence_count; k++){
 					text[k]=text[k+1];
@@ -46,8 +49,7 @@ char** readText(int *sentence_count){
 				}
 				j--;
 			}
-	text = (char**)realloc(text, *sentence_count);
-	return text;
+    return text;
 }
 
 void printText(char **text, int sentence_count){
@@ -56,36 +58,37 @@ void printText(char **text, int sentence_count){
 	return;
 }
 
-int isVowel(char* ch){
-	char vowels[] = "aeiouy";
-	if (strstr(vowels, ch)==NULL)
+int isVowel(char ch){
+	const char vowels[] = "aeiouyAEIOUY";
+	if (strchr(vowels, ch)==NULL)
 		return 0;
 	return 1;
 }
 
 void changeVowel(char **text, int sentence_count){
 	for(int i=0; i<sentence_count; i++)
-		for(int j=0; j<strlen(text[i]); i++)
-			if(isVowel(&text[i][j]))
+		for(int j=0; j<strlen(text[i]); j++)
+			if(isVowel(text[i][j]))
 				text[i][j]++;
 	return;
 }
+
 
 int isToBe(char* sentence){
 	char mask[]="To * or not to *.";
 	int length[2] = {0};
 	int wordCount=0;
 	int j=0;
-	for(int i=0; i<17; i++){
-		if(mask[j]='*'){
-			while(sentence[i]!=' '){
-				i++;
+	for(int i=0; i<strlen(mask); i++){
+		if(mask[i]=='*'){
+			while(isalnum(sentence[j])){
+				j++;
 				length[wordCount]++;
 			}
-			j++;
+			i++;
 			wordCount++;
 		}
-		if(sentence[i]!=mask[j])
+		if(sentence[j]!=mask[i])
 			return 0;
 		j++;
 	}
@@ -95,35 +98,37 @@ int isToBe(char* sentence){
 }
 
 void findToBe(char **text, int *sentence_count){
-	int length;
+	int length=0;
 	char *wordFirst = (char*)calloc(1, sizeof(char));
 	char *wordSecond = (char*)calloc(1, sizeof(char));
-	int j=0;
 	for(int i=0; i<*sentence_count; i++){
 		length = isToBe(text[i]);
-		if(length!=0){
-			if(length<0){
-				length = -length;
-				wordFirst = (char*)realloc(wordFirst, length+1);
-				for(j=0; j<length; j++)
-					wordFirst[j]=text[i][3+j];
-				wordFirst[length]='\0';
-				text[i]=realloc(text[i], length+1);
-				strcpy(text[i], wordFirst);
-			}else{
-				wordSecond=(char*)realloc(wordSecond, length+1);
-				for(j=0; j<length; j++)
-					wordSecond[length-1-j]=text[i][strlen(text[i]-2-j)];
-				wordSecond[length]='\0';
-				text[i]=realloc(text[i], length+1);
-				strcpy(text[i], wordSecond);
+		if(length<0){
+			length = -length;
+			wordFirst = (char*)realloc(wordFirst, length+1);
+			for(int j=0; j<length; j++)
+				wordFirst[j]=text[i][3+j];
+			wordFirst[length]='\0';
+			text[i]=realloc(text[i], length+1);
+			strcpy(text[i], wordFirst);
+			continue;
+		}
+		if(length>0){
+			wordSecond=(char*)realloc(wordSecond, length+1);
+			for(int j=0; j<length; j++)
+				wordSecond[length-1-j]=text[i][strlen(text[i])-2-j];
+			wordSecond[length]='\0';
+			text[i]=realloc(text[i], length+1);
+			strcpy(text[i], wordSecond);
+			continue;
 			}
-		}else{
+		if(length==0){
 			free(text[i]); ////////////////
 			(*sentence_count)--;
-			for(int k=i; k<*sentence_count; k++)
-				text[k]=text[k+1];
+			for(int j=i; j<*sentence_count; j++)
+				text[j]=text[j+1];
 			i--;
+			continue;
 		}
 	}
 	return;
@@ -164,26 +169,29 @@ void backwardText(char **text, int sentence_count){
 			k--;
 		}
 	}
+	return;
 }
 
-int mystrcmp(const char** first, const char** second){
+int mystrcmp(const void* first, const void* second){						////WTF IT DOESN'T WORK
 	int i1=0;
 	int i2=0;
+	first=(char*)first;
+	second=(char*)second;
 	while(1){
-		if((*first[i1]=='.')&&(*second[i2]=='.'))
+		if((first[i1]=='.')&&(second[i2]=='.'))
 			return 0;
-		if((*first[i1]==' ')||(*first[i1]==','))
+		if((first[i1]==' ')||(first[i1]==','))
 			i1++;
-		if((*second[i2]==' ')||(*second[i2]==','))
+		if((second[i2]==' ')||(second[i2]==','))
 			i2++;
-		if(*first[i1]=='.')
-			return (int)(*second[i2]);
-		if(*second[i2]=='.')
-			return (int)(*first[i1]);
-		if(*first[i1]==*second[i2]){
+		if(first[i1]=='.')
+			return second[i2];
+		if(second[i2]=='.')
+			return first[i1];
+		if(first[i1]==second[i2]){
 			i1++;
 			i2++;
-		} else return (int)(*first[i1]-*second[i2]);
+		} else return first[i1]-second[i2];
 	}
 }
 
@@ -194,23 +202,25 @@ void sortLast(char **text, int sentence_count){
 	return;
 }
 
+
 int main(){
-	int command;
-	int sentence_count = 0;
-	char **text = readText(&sentence_count);
-	if(text==NULL){
+    int sentence_count=0;
+    int command;
+    char** text = readText(&sentence_count);
+    if(text==NULL){
 		printf("Ошибка выделения памяти.\n");
 		return 1;
 	}
 	printf("Введите одно из чисел ниже, чтобы выбрать действие:\n\n1) Заменить в тексте все гласные буквы на следующую букву в алфавите.\n2) Найти все предложения вида “To <подстрока1> or not to <подстрока2>” и для каждого такого предложения вывести подстроку у которой длина больше.\n3) Удалить все предложения у которых длина первого слова равняется 4.\n4) Отсортировать предложения по увеличению кода последнего символа не являющегося разделителем предложений или слов.\n0) Выйти из программы.\n");
 	scanf("%d", &command);
+	printf("\n");
 	switch(command){
-		case 1:
+	    case 1:
 			changeVowel(text, sentence_count);
 			printText(text, sentence_count);
 			break;
 		case 2:
-			findToBe(text, &sentence_count);
+		    findToBe(text, &sentence_count);
 			printText(text, sentence_count);
 			break;
 		case 3:
